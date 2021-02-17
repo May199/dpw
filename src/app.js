37fc735c2e15149eb9665d20a98e15d05913cb76
Emanuelle Neves <43800901+May199@users.noreply.github.com>
2021-02-17 15:16:33 -0300

    em andamento

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+
+app.set('views', __dirname +'/views/');
+app.set('view engine', 'ejs');
+app.use(expressLayout);
+app.use(express.static(__dirname + '/public/')); //quando colocar essa pasta como static, usa apenas o caminho apartir daqui
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.render('pages/home');
+});
+
+app.listen(process.env.PORT || 3000);
